0ff246c38b14141dd130a482bd571b16f77cd6df
mk020846 <132980253+mk020846@users.noreply.github.com>
2023-11-10 22:00:40 +0530

    Improving the code by using classes for encapsulation.
    Improvements:
    
        Class-based approach:
        I introduced a PdfToMp3Converter class to encapsulate the functionality. This makes the code more modular and easier to understand.
    
        Comments:
        Added comments to explain the purpose of each method and to provide more clarity on the code's functionality.
    
        Refactoring:
        Removed redundant code and improved the structure for better readability.
    
    By organizing your code into a class, it becomes more maintainable and extensible. It also allows for easier reuse of the code in other parts of your project.Hope you'll like it and admire my approach.

--- main.py
+++ main.py
@@ -3,32 +3,44 @@ import pdfplumber
 from art import tprint
 from pathlib import Path
 
-def pdf_to_mp3(file_path='test.pdf', language ='en'):
-    if Path(file_path).is_file() and Path(file_path).suffix == '.pdf':
+class PdfToMp3Converter:
+    def __init__(self, file_path='test.pdf', language='en'):
+        self.file_path = Path(file_path)
+        self.language = language
 
-        print('[+] Processing...')
-        with pdfplumber.PDF(open(file=file_path, mode ='rb')) as pdf:
-            pages = [page.extract_text() for page in pdf.pages]
+    def check_file_validity(self):
+        """Check if the file exists and is a valid PDF."""
+        return self.file_path.is_file() and self.file_path.suffix.lower() == '.pdf'
 
-        text = ''.join(pages)
-        text = text.replace('\n', '')
+    def process_pdf_to_mp3(self):
+        """Process the PDF, extract text, convert to MP3, and save the audio file."""
+        if self.check_file_validity():
+            print('[+] Processing...')
+            with pdfplumber.open(self.file_path) as pdf:
+                pages = [page.extract_text() for page in pdf.pages]
 
-        my_audio = gTTS(text=text, lang=language, slow=False)
-        file_name = Path(file_path).stem
-        my_audio.save(f'{file_name}.mp3')
+            text = ''.join(pages)
+            text = text.replace('\n', '')
 
-        return f'[+] {file_name}.mp3 saved succeessfully!'
-    else:
-        return "File not exists, check the file"
+            audio_file = self.file_path.stem + '.mp3'
+            my_audio = gTTS(text=text, lang=self.language, slow=False)
+            my_audio.save(audio_file)
+
+            return f'[+] {audio_file} saved successfully!'
+        else:
+            return "Invalid file. Please provide a valid PDF file."
 
 def main():
-    tprint('PDF>>TO>>MP3', font='bulbhead')
-    file_path = input("\nEnter a file's path:")
-    language = input("Choose language for example 'en' or 'ru': ")
-    print(pdf_to_mp3(file_path=file_path, language=language))
+    tprint('PDF >> TO >> MP3', font='bulbhead')
+    file_path = input("\nEnter a file's path: ")
+    language = input("Choose language, for example, 'en' or 'ru': ")
+
+    # Create an instance of the PdfToMp3Converter class
+    converter = PdfToMp3Converter(file_path=file_path, language=language)
+
+    # Process PDF to MP3 and print the result
+    result = converter.process_pdf_to_mp3()
+    print(result)
 
 if __name__ == '__main__':
     main()
-
-
-   
